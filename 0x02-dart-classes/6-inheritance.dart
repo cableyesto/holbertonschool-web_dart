8ff43a94567a02ae82dca48578d63cbcb5ce3a42
cableyesto <dialkins@tutamail.com>
2026-04-13 12:33:04 +0200

    update private property.

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -5,15 +5,21 @@ class User extends Password {
   String name;
   int age;
   double height;
-  String? user_password;
+  String? _user_password;
 
   User({
     required this.id,
     required this.name,
     required this.age,
     required this.height,
-    this.user_password,
-  }) : super(password: user_password ?? '');
+    String? user_password,
+  })  : _user_password = user_password,
+        super(password: user_password ?? '');
+
+  set user_password(String value) {
+    _user_password = value;
+    password = value;
+  }
 
   Map<String, Object> toJson() {
     return {
